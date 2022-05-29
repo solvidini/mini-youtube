be162f701ea8c289283c9e9357ce91ef7558995a
Samuel Kedziora <samukedo@gmail.com>
2022-05-29 02:05:49 +0200

    API cleanup

--- src/api/youtube.ts
+++ src/api/youtube.ts
@@ -1,7 +1,4 @@
-export const API_KEY = 'AIzaSyB3nKA1tbpQqkgICsHh3zaB3enGQgEmF2A'
-// export const API_KEY = 'AIzaSyDWwmGhpjhskhGwRaesIEX8_db6Ro2G6C8'
-
-// export const API_KEY = 'AIzaSyBi_vwqaqk_x-YgXkcaMAFZkEkMgkhjzFQ'
+export const API_KEY = 'AIzaSyBi_vwqaqk_x-YgXkcaMAFZkEkMgkhjzFQ'
 export const BASE_URL = 'https://www.googleapis.com/youtube/v3'
 export const CHANNEL_URL = 'https://www.youtube.com/channel/'
 export const EMBED_URL = 'https://www.youtube.com/embed/'
